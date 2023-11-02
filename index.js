22d3206e490338922ef18d6645523ae0a10dfcea
Jiří Kofránek <kofranek@gmail.com>
2023-11-02 15:57:02 +0100

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+//hodnota čítače
+let counter=0
+
+//hledání prvku html
+const plusButton = document.getElementById('plus')
+const minusButton = document.getElementById('minus')
+const counterValue = document.getElementById('counterValue')
+
+//manipulace s prvky html
+counterValue.textContent=counter
+
+plusButton.addEventListener('click',increment)
+minusButton.addEventListener('click',decrement)
+
+//obslužná fukce
+function increment() {
+  counter++ 
+  console.log('counter=', counter)
+}
+
+function decrement() {
+    counter-- 
+    console.log('counter=', counter)
+  }
